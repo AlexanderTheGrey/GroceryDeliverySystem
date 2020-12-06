36cef0241679740819292ade8a2acf218fabb16e
Christopher De Leon <18326238+CDeLeon94@users.noreply.github.com>
2020-12-05 18:21:28 -0600

    Working search, added productlist.js

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Product from "./Product";
+import productlist from './productlist'
 
 function Home() {
   return (
@@ -12,22 +13,16 @@ function Home() {
         />
 
         <div className="home_group">
-          <Product
-            id="00000001"
-            name="Big Bennie's Broccoli"
-            image="https://images-na.ssl-images-amazon.com/images/I/71mYzDfK-6L._SL1024_.jpg"
-            price={4.99}
-            rating={2.5}
-            description="Rich green broccoli"
-          />
-          <Product
-            id="00000002"
-            name="Fryer Whole Chicken"
-            image="https://images-na.ssl-images-amazon.com/images/I/612jiEdQ58L._SL1000_.jpg"
-            price={9.99}
-            rating={3.5}
-            description="Juicy chicken"
-          />
+        {productlist.map((pd) =>(
+            <Product
+              id={pd.id}
+              name={pd.name}
+              image={pd.image}
+              price={pd.price}
+              rating={pd.rating}
+              description={pd.description}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,29 @@ import React from "react";
 import "./Home.css";
 import Product from "./Product";
 import { useStateValue } from "./StateProvider";
+import productlist from './productlist'
 
 function Search() {
   const [{ searchTerm }, dispatch] = useStateValue();
-  let currSearch = searchTerm;
+  // getSearchReults
+  let searchResults = productlist.filter(pd => 
+    pd.name.toLowerCase().includes(searchTerm) ||
+    pd.description.toLowerCase().includes(searchTerm))
+
   return (
     <div className="search">
-      {currSearch}
+      
+
+      {searchResults.map(item => (
+            <Product
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              quantity={item.quantity}
+            />
+          ))}
     </div>
   );
 }

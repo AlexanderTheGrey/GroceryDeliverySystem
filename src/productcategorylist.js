b1b993d0eca06a38115955e3a962ac1bc9706739
seymaoz <61207223+seymaoz@users.noreply.github.com>
2020-12-06 19:20:55 -0600

    Update product categories

--- src/productcategorylist.js
+++ src/productcategorylist.js
@@ -1,9 +1,5 @@
 let categories = 
 [
-<<<<<<< HEAD
 	"Meat & Seafood","Drinks","Vegetables","Fruit","Bread","Dairy","Cheese","Household","Egg","Canned & Packaged"
-=======
-	"meat","produce"
->>>>>>> 408dcef619b48738237d0500f2ba3dac71bf0648
 ];
 export default categories;
